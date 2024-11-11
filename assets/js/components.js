34561d4aeee76df5b7898f922b00c7aa9e6c08da
MrSumitKumar <72176567+MrSumitKumar@users.noreply.github.com>
2024-11-11 15:01:30 +0530

    update with responsive

--- assets/js/components.js
+++ assets/js/components.js
@@ -4,5 +4,24 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(data => {
             document.getElementById('curriculum').innerHTML = data;
         });
-    
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('../assets/components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        });
+});
+
+const curriculum = document.getElementById('curriculum');
+
+function curriculum_open_btn() {
+    curriculum.style.width = '300px';
+    curriculum.style.display = 'block';
+}
+
+function curriculum_close_aside() {
+    curriculum.style.width = '0';
+    curriculum.style.display = 'none';    
+}
